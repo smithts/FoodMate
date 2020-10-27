71912312c4f3c13c05e77ef2d031e7aced31d072
Tanner Smith <tanner.smith1125@gmail.com>
2020-10-27 19:30:32 -0400

    Initial Commit

---
+++ FoodMate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodMate
+//
+//  Created by Tanner Smith on 10/27/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FoodMate/FoodMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodMateApp.swift
+//  FoodMate
+//
+//  Created by Tanner Smith on 10/27/20.
+//
+
+import SwiftUI
+
+@main
+struct FoodMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
