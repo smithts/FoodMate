0bcec4a90e3aa60e40b7749a67b8b092cdea180b
Neil Richard King Rice <kingrice@Neils-MacBook-Pro-2.local>
2020-11-22 16:48:35 -0500

    Added icon images for app aand created a class for fetching remote images.

--- FoodMate/Views/ItemDetail.swift
+++ FoodMate/Views/ItemDetail.swift
@@ -4,16 +4,20 @@
 //
 //  Created by Tanner Smith on 11/5/20.
 //
-
 import SwiftUI
 
 struct ItemDetail: View {
-    var item: FoodItem
     
+    let jsonURL = "https://images.barcodelookup.com/3181/31817127-1.jpg"
+
+    var item: FoodItem
     var body: some View {
+        RemoteImage(url: jsonURL)
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 200)
         Text(item.name)
-            .font(.largeTitle)
-            .foregroundColor(.red)
+                    .font(.largeTitle)
+                    .foregroundColor(.red)
     }
 }
 
